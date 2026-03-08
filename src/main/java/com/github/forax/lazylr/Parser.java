fad7de79b86e171df3d8adbcae498a301c41a4dd
forax <forax@univ-mlv.fr>
2026-03-08 22:53:34 +0100

    remove unused imports

--- src/main/java/com/github/forax/lazylr/Parser.java
+++ src/main/java/com/github/forax/lazylr/Parser.java
@@ -12,9 +12,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.Objects;
-import java.util.Optional;
 import java.util.Set;
-import java.util.stream.Stream;
 
 /// The primary engine for performing LR(1) parsing.
 ///
