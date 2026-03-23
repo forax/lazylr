53b8a3329bad75c48825352056c27a6f7f3caa36
forax <forax@univ-mlv.fr>
2026-03-23 12:32:10 +0100

    remove unused import

--- src/main/java/com/github/forax/lazylr/LRTransitionEngine.java
+++ src/main/java/com/github/forax/lazylr/LRTransitionEngine.java
@@ -9,7 +9,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
-import java.util.StringJoiner;
 import java.util.stream.Collectors;
 
 /// Manage state transitions and resolve grammar conflicts using precedence.
